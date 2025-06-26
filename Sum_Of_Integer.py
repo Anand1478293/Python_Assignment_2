7ddb74164be99dcff6130aead21c651cc9e9f6e1
Anand1478293 <Sharma.anand.8218@gmail.com>
2025-06-26 08:09:35 +0530

    Add files via upload

---
+++ Sum_Of_Integer.py
@@ -0,0 +1,12 @@
+# Sum of Integer from 1 to 50
+
+sum = 0
+
+# use for loop to get from 1 to 50 
+
+for i in range(1, 51):
+    sum += i
+
+# printing result
+
+print("The sum of number from 1 to 50 is:",sum)
